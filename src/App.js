cff41dc628a50ad626c5e885372da0945790c20c
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-04 14:32:19 +0545

    Create: navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import About from "./components/About";
 import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
-      <About></About>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/" exact>
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/todo">
+            <Todo></Todo>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/todo">Todo</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
