7b3d012464bb33d6c059603e2910da67f676c862
Asim Shrestha <asimshrestha608@gmail.com>
2021-09-05 15:18:37 +0545

    Remove: console.log()

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -5,7 +5,6 @@ import TodoDetail from "./TodoDetail";
 
 const TodoList = () => {
   const { todos } = useContext(TodoContext);
-  console.log(todos);
   return (
     <ul>
       {todos.map((todo) => (
